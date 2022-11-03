e520ba375ac203be3c5b968987041144ee6192d2
Harsh <harsh9607@gmail.com>
2022-11-03 20:11:56 +0530

    PRIME NUMBERS IN A RANGE

---
+++ main (6).cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include<cmath> 
+
+using namespace std;
+
+int main()
+{
+    // print all primes in a range
+    int low , high  ;
+    cout<<"Enter low and high :";
+    cin>>low>>high; 
+    
+   
+        for(int j= low ; j<=high ; j++ )
+        {   int flag = 0 ; 
+        
+            for( int i=2 ; i<= sqrt(j) ; i++ )
+            {
+                
+                if( j%i == 0 )
+                {
+                    flag = 1 ;
+                }
+                
+            
+            } 
+            
+            if(flag == 0 )
+            {
+                cout<<j<<endl; 
+            }
+        
+        }
+    
+    return 0;
+}
